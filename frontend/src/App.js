0d2cb43539c5359e9a03b522c9b0d054d2a83567
Jeffrey Yu <35394596+JeffreytheCoder@users.noreply.github.com>
2023-04-22 15:58:28 -0700

    fix react-pdf babel-loader issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
-import { Document, Page } from 'react-pdf';
+import React, { useEffect, useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 
@@ -13,8 +13,12 @@ const options = {
 };
 
 function App() {
+  useEffect(() => {
+    pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+  });
+
   const [numPages, setNumPages] = useState();
-  const [file, setFile] = useState("./test.pdf");
+  const [file, setFile] = useState('/test.pdf');
   function onFileChange(event) {
     const { files } = event.target;
 
@@ -27,24 +31,28 @@ function App() {
   }
 
   return (
-      <div className="Example">
-        <header>
-          <h1>react-pdf sample page</h1>
-        </header>
-        <div className="Example__container">
-          <div className="Example__container__load">
-            <label htmlFor="file">Load from file:</label>{' '}
-            <input onChange={onFileChange} type="file" />
-          </div>
-          <div className="Example__container__document">
-            <Document file={file} onLoadSuccess={onDocumentLoadSuccess} options={options}>
-              {Array.from(new Array(numPages), (el, index) => (
-                  <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-              ))}
-            </Document>
-          </div>
+    <div className="Example">
+      <header>
+        <h1>react-pdf sample page</h1>
+      </header>
+      <div className="Example__container">
+        <div className="Example__container__load">
+          <label htmlFor="file">Load from file:</label>{' '}
+          <input onChange={onFileChange} type="file" />
+        </div>
+        <div className="Example__container__document">
+          <Document
+            file={file}
+            onLoadSuccess={onDocumentLoadSuccess}
+            options={options}
+          >
+            {Array.from(new Array(numPages), (el, index) => (
+              <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+            ))}
+          </Document>
         </div>
       </div>
+    </div>
   );
 }
 
