92bb645038c6d128f9cf24e5a07bcd4b0978cab0
xhsuj <yhbxhsuj@163.com>
2023-04-23 03:45:24 -0700

    upload document dropzone, connect with api, add cors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Slides from './components/Slides';
+import Upload from './components/Upload';
 import React from 'react';
+import { BotProvider } from './contexts/BotContext';
 
 function App() {
-  return <Slides />;
+  return <BotProvider><Upload /></BotProvider>;
 }
 
 export default App;
